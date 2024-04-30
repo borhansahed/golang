9dde166877e110124f98a5f8cad2a7f309153a38
borhansahed <mohammadborhan022@gmail.com>
2024-04-30 18:18:36 +0600

    variable

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"learning/helpers"
 )
 
 func main() {
 	fmt.Println("Hello Sahed bhai")
-    helpers.HelloWorld("Microservice shikbi go diya taka kamabi")
+
 }
---
+++ variable/variable.go
@@ -0,0 +1,37 @@
+package variable
+
+import "fmt"
+
+
+func Variables () {
+		// constant variable
+		const str string = "sahed";
+
+		// int
+		var num int = 23;
+	
+		// int unsigned
+		var num1 uint= 34;
+	
+	
+		// float 
+		var f float32 = 3.4
+		var f1 float64 = 32.4
+	
+		// short hand to declared variable
+	
+		person := "sahed"
+	
+		
+	
+		// compiler will set the variable type
+		var normal = 23;
+	
+		fmt.Println(str);
+		fmt.Println(num)
+		fmt.Println(num1)
+		fmt.Println(f)
+		fmt.Println(f1)
+		fmt.Println(person)
+		fmt.Println(normal)
+}
