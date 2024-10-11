04ca2a1ebfec18cf4cf68917da3d59baa7a5fb30
borhansahed <mohammadborhan022@gmail.com>
2024-10-11 23:39:22 +0600

    waitgroup

--- advanced/goroutines/goroutines.go
+++ advanced/goroutines/goroutines.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 // goroutine is a lightweight thread managed by the Go runtime.
@@ -14,14 +14,27 @@ func task (i int) {
 
 func main() {
 
+	var wg sync.WaitGroup
+
 	// go keyword  use to run the function as a goroutine.
 	for i:= 0; i<10; i++ {
+
+		wg.Add(1)
 		// anonymous function
-		go func(i int) {
+		go func(i int, wg * sync.WaitGroup) {
 			fmt.Println("running task",i)
-		}(i)
+			wg.Done()
+		}(i, &wg)
 	}
 	
 	// wait for all goroutines to finish
-	time.Sleep(time.Second * 2)
+	// time.Sleep(time.Second * 2) 
+
+	// We can't give time.sleep to main function because it will block the main function and the program will not exit. we also don't know how much time to wait.
+
+	
+	// we use waitgroup to wait for all goroutines to finish.
+
+	wg.Wait()
+	
 }
